f2c4b5d19417663e3ab415c8cdbb04f71df41eb7
Mehran <mehransurface@outlook.com>
2021-12-12 21:51:22 +0330

    next test - pages - and test deployment on Arvancloud

---
+++ dropship/pages/login/index.js
@@ -0,0 +1,6 @@
+function LoginPage(){
+  return  <h1>The News Page</h1>
+}
+
+
+export  default LoginPage;
---
+++ dropship/pages/news/index.js
@@ -0,0 +1,9 @@
+// our-domain.com/news
+// import  React from  'react'; we don't need this import on next js
+
+function NewsPage(){
+  return  <h1>The News Page</h1>
+}
+
+
+export  default NewsPage;
