9e5c68f34503332e2a373a14bfdefb3fb7315cb2
Mehran <mehransurface@outlook.com>
2021-12-27 19:15:25 +0330

    update newspage component

--- dropship/pages/news/index.js
+++ dropship/pages/news/index.js
@@ -1,8 +1,21 @@
 // our-domain.com/news
 // import  React from  'react'; we don't need this import on next js
+import  Link  from  'next/link';
+import { Fragment } from "react";
 
 function NewsPage(){
-  return  <h1>The News Page</h1>
+  return( 
+    <Fragment>
+    <h1>The News Page</h1>
+    <ul>
+      <li><Link href='/news/online-shoping-story'>online shoping</Link></li>
+      <li>Somthing Else</li>
+
+    </ul>
+    </Fragment> 
+  )
+      
+
 }
 
 
